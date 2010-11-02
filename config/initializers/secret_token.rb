2e3461874d05de388df057b047117c766275c6fa
Pablo Villalba <pablo@teambox.com>
2010-11-02 12:23:29 +0100

    Initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeamboxGuide::Application.config.secret_token = 'ef7fa2867af97931f922011bc68594ad71f0c24d39438923ba5f4b7304dc7783e9f37c710897db98a80b9e104bb6ed72a3dd93368523b1621bdf58f7599690b4'
