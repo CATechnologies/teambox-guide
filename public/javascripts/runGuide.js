ac686f0229b00f6a9be80fc5cbadc216c40151b7
Pablo Villalba <pablo@teambox.com>
2010-11-03 16:37:12 +0100

    Inline images, reviewing and editing the guide

--- public/javascripts/runGuide.js
+++ public/javascripts/runGuide.js
@@ -2,5 +2,9 @@ jQuery(document).ready(function($) {
   $('a[rel*=facebox]').facebox({
     loading_image : 'loading.gif',
     close_image   : 'closelabel.gif'
-  }) 
+  })
+  
+  $('img.screenshot').bind('click', function() {
+    $(this).animate({width: "700px"}, 1000)
+  })
 })
